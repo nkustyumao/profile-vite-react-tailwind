d1b710b65aed93d08bb7bac00da0c37ea006fee3
nkustyumao <f109152172@nkust.edu.tw>
2025-02-23 13:14:02 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/vite-react-project/",
-=======
   base: "/profile-vite-react-tailwind/",
->>>>>>> 30fa546af1b49acde2f4f1a7abb7fecddcfc2147
   plugins: [react(), tailwindcss()],
 })
